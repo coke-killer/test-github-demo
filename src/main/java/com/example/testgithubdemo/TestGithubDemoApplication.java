1331cd9cb02f922af175bec3456c12fcb7602ca7
yudongyue <oakyue@outlook.com>
2020-10-26 21:57:23 +0800

    test

---
+++ src/main/java/com/example/testgithubdemo/TestGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGithubDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testgithubdemo/TestGithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgithubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
